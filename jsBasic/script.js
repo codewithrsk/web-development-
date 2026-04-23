62fcad9b7d465fe4d4883ae22b4ebc6f2402ecd1
codewithrsk <rsk800001@gmail.com>
2026-04-23 10:38:12 +0530

    Add initial HTML structure and JavaScript file for basic webpage setup

---
+++ jsBasic/script.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+let x=10;
+x=30;
+
+var p=12;
+var p=12;
