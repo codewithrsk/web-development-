96cc3701cb734e223851d0c23ac7eb85033753f6
codewithrsk <rsk800001@gmail.com>
2026-04-29 19:07:55 +0530

    Refactor script.js: comment out unused loop and log array elements

--- jsloops/script.js
+++ jsloops/script.js
@@ -1,4 +1,9 @@
-for (var i = 1; i <= 10; i++) {
-  // console.log("2*" i )
-  console.log(2 * i);
+// for (var i = 1; i <= 10; i++) {
+//   // console.log("2*" i )
+//   console.log(2 * i);
+// }
+
+let a = [24,58,33,86,96,12,53,23,32,100]
+for(var i= 0; i<10 ;i++){
+    console.log(a[i])
 }
