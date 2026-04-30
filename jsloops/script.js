5c5cfa4e99bfa1abc80634ca17e1e6e4c9c73a9d
codewithrsk <rsk800001@gmail.com>
2026-04-30 13:15:06 +0530

    Add HTML and JavaScript files for multiple assignments; implement basic functionality and console logging

--- jsloops/script.js
+++ jsloops/script.js
@@ -1,5 +1,5 @@
 // for (var i = 1; i <= 10; i++) {
-//   // console.log("2*" i )
+//   // console.log("2*", i )
 //   console.log(2 * i);
 // }
 
@@ -7,3 +7,14 @@ let a = [24,58,33,86,96,12,53,23,32,100]
 for(var i= 0; i<10 ;i++){
     console.log(a[i])
 }
+var i=1;
+while(i<=10){
+    console.log("2*",i);
+    i++;
+}
+
+var i=1;
+do{
+    console.log(2*i);
+    i++;    
+}while (i<=10)
