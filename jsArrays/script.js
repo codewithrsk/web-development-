7b801c41bcd4484b277bfa92f4893ca8c5125e11
codewithrsk <rsk800001@gmail.com>
2026-05-06 18:27:14 +0530

    Add index.html and script.js files; implement array iteration and logging

---
+++ jsArrays/script.js
@@ -0,0 +1,45 @@
+// let arr = [4, 12, 48, 79, 799,45, 77];
+// for (var i = 0; i < 7; i++) {
+//   console.log(arr[i]);
+// }
+// console.log(arr);
+// arr[5] = arr[5] + 9;
+// console.log(arr);
+// arr=[]
+// console.log(arr);
+
+let arr = [12, 23, 34, 45, 56, 67, 78, 89, 90];
+// console.log("og  arrays ");
+// console.log(arr);
+
+// arr.push(99);
+// console.log("arrays after push");
+
+// console.log(arr);
+// arr.pop();
+// console.log("arrays after pop");
+
+// console.log(arr);
+// arr.unshift(11);
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+
+// console.log(arr.at(5));
+// console.log(arr.indexOf(23));
+// console.log(arr.includes(999));
+// console.log(arr.indexOf(999));
+
+// // console.log(arr.findi);
+// let uar =[56,23,89,45,67,34,12,90,87,45,98]
+// console.log(uar);
+// uar.sort()
+// console.log(uar);
+// uar.reverse()
+// console.log(uar);
+// uar.length()
+// console.log(uar);
+
+arr.forEach((element) => {
+  console.log(element);
+});
