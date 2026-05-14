78a54029b40945ea5d07af39c58a9c0f66708ee8
codewithrsk <rsk800001@gmail.com>
2026-05-14 20:21:01 +0530

    update in Institute Registration Form html and js

--- Projects/CoachingInstituteRegistrationForm/script.js
+++ Projects/CoachingInstituteRegistrationForm/script.js
@@ -1,5 +1,32 @@
-document.getElementById("submit", (event)=>{
-    const fn =document.getElementById("fullName").value 
-    console.log(fn);
-    
-})
+// SubmitButton
+document
+  .getElementById("registrationFrom")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const fn = document.getElementById("fullName").value;
+    const mail =document.getElementById("email").value;
+    const PHONE=document.getElementById("phone").value
+    const gndr = document.querySelector("input[name='gender']:checked")?.value;
+
+    const timings = [];
+
+    document
+      .querySelectorAll("input[name='timings']:checked")
+      .forEach((item) => {
+        timings.push(item.value);
+      });
+      
+    console.log(gndr);
+
+    console.log(timings);
+  });
+
+// ResetButton
+document
+  .getElementById("registrationFrom")
+  .addEventListener("reset", (event) => {
+    event.preventDefault();
+
+    window.location.reload();
+  });
