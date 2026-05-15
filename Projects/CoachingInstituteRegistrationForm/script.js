37b02c0cd9277dfe1a618d8241303ea29664eb7f
codewithrsk <rsk800001@gmail.com>
2026-05-15 20:31:53 +0530

    add local Stroge in To do List

--- Projects/CoachingInstituteRegistrationForm/script.js
+++ Projects/CoachingInstituteRegistrationForm/script.js
@@ -4,23 +4,50 @@ document
   .addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const fn = document.getElementById("fullName").value;
-    const mail =document.getElementById("email").value;
-    const PHONE=document.getElementById("phone").value
-    const gndr = document.querySelector("input[name='gender']:checked")?.value;
+    
+    
+
+    const fullName = document.getElementById("fullName").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const dob = document.getElementById("dob").value;
+    const gender = document.querySelector("input[name='gender']:checked")?.value;
+    const qualification = document.getElementById("qualification").value ;
+    const marks = document.getElementById("marks").value;
+    const course = document.getElementById("course").value;
+    const address = document.getElementById("address").value;
+    const city = document.getElementById("city").value;
+    const pin = document.getElementById("pin").value;
 
     const timings = [];
-
     document
       .querySelectorAll("input[name='timings']:checked")
       .forEach((item) => {
         timings.push(item.value);
       });
-      
-    console.log(gndr);
 
-    console.log(timings);
+    const registrationData = {
+      FullName: fullName,
+      Email: email,
+      Phone: phone,
+      DateofBirth: dob,
+      Gender: gender,
+      Qualification: qualification,
+      Marks: marks,
+      Course: course,
+      Timings: timings,
+      Address: address,
+      City: city,
+      PinCode: pin,
+    };
+
+    validateData(registrationData)
+      ? (console.log("Registration Data:", registrationData),
+        alert("Registration successful! Check console for details."))
+      : alert("Registration unsuccessfull! Enter valid Data.");
   });
+  
+
 
 // ResetButton
 document
@@ -30,3 +57,50 @@ document
 
     window.location.reload();
   });
+
+function validateData(data) {
+  document.querySelectorAll(".manualError").forEach((item) => {
+    item.innerText = "";
+  });
+  console.log(data);
+  
+  let isValid = true;
+
+  console.log(data);
+
+  if (!data.FullName) {
+    document.getElementById("fullNameError").innerText = "Full Name Required";
+    isValid = false;
+  } else if (!/^[A-Za-z\s]+$/.test(data.FullName)) {
+    document.getElementById("fullNameError").innerText =
+      "Only alphabets and spaces are allowed";
+    isValid = false;
+  }
+
+  if (!/^[A-Za-z\d\.\_]+@+(gmail.com|outlook.com|ricr.in)$/.test(data.Email)) {
+    document.getElementById("emailError").innerText =
+      "Only standard Email format allowed with @gmail.com";
+    isValid = false;
+  }
+
+  if (!/^[6-9]\d{9}$/.test(data.Phone)) {
+    document.getElementById("phoneError").innerText =
+      "Only Indian mobile numbers allowed";
+    isValid = false;
+  }
+
+  console.log(new Date().getFullYear,"curent year");
+    console.log(data.DateofBirth.slice('-')[0],"user year");
+
+  if(new Date().getFullYear-data.DateofBirth.slice('-')[0]<14){
+    console.log(new Date().getFullYear,"curent year");
+    console.log(data.DateofBirth.slice('-')[0],"user year");
+    
+    document.getElementById("dobError").innerText="GROW Up"
+    isValid=false;
+  }
+
+ 
+
+  return isValid;
+}
--- Projects/To-Do-List/script.js
+++ Projects/To-Do-List/script.js
@@ -39,6 +39,59 @@ function addNewTask() {
   document.getElementById("taskDiv").classList.remove("d-none");
 
   document.getElementById("taskList").appendChild(LI);
+  saveToLocalStroge(taskToAdd)
 
   document.getElementById("newTask").value = "";
 }
+
+function saveToLocalStroge(task){
+  const TaskArray= JSON.parse(localStorage.getItem("ToDoTask")) || []
+
+  TaskArray.array.forEach((taskToAdd) => {
+    const LI = document.createElement("li");
+  LI.classList.add("my-3");
+
+  const DIV = document.createElement("div");
+  DIV.classList.add("d-flex", "align-items-center");
+
+  const SPAN1 = document.createElement("span");
+  SPAN1.innerText = taskToAdd;
+
+  const BUTTON = document.createElement("button");
+  BUTTON.classList.add(
+    "btn",
+    "btn-danger",
+    "ms-4",
+    "d-flex",
+    "gap-3",
+    "align-items-center",
+    "justify-content-center",
+  );
+  BUTTON.onclick = () => LI.remove();
+
+  const I = document.createElement("i");
+  I.classList.add("bi", "bi-trash");
+
+  const SPAN2 = document.createElement("span");
+  SPAN2.innerText = "Delete";
+
+  BUTTON.appendChild(I);
+  BUTTON.appendChild(SPAN2);
+
+  DIV.appendChild(SPAN1);
+  DIV.appendChild(BUTTON);
+
+  LI.appendChild(DIV);
+
+  document.getElementById("taskDiv").classList.remove("d-none");
+
+  document.getElementById("taskList").appendChild(LI);
+    
+  });
+
+
+  TaskArray.push(task)
+  localStorage.setItem("ToDoTask",TaskArray)
+}
+
+
