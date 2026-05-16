fdc144a017e7771bfaa3820f2e4f3f1af559953b
codewithrsk <rsk800001@gmail.com>
2026-05-16 23:35:07 +0530

    creatiing password manager

--- Projects/To-Do-List/script.js
+++ Projects/To-Do-List/script.js
@@ -20,7 +20,10 @@ function addNewTask() {
     "align-items-center",
     "justify-content-center",
   );
-  BUTTON.onclick = () => LI.remove();
+  BUTTON.onclick = () => {
+    LI.remove();
+    removeTaskFromLocalStorage(taskToAdd);
+  };
 
   const I = document.createElement("i");
   I.classList.add("bi", "bi-trash");
@@ -39,59 +42,86 @@ function addNewTask() {
   document.getElementById("taskDiv").classList.remove("d-none");
 
   document.getElementById("taskList").appendChild(LI);
-  saveToLocalStroge(taskToAdd)
+
+  saveToLocalStorage(taskToAdd);
 
   document.getElementById("newTask").value = "";
 }
 
-function saveToLocalStroge(task){
-  const TaskArray= JSON.parse(localStorage.getItem("ToDoTask")) || []
+function saveToLocalStorage(taskToAdd) {
+  const TaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
 
-  TaskArray.array.forEach((taskToAdd) => {
-    const LI = document.createElement("li");
-  LI.classList.add("my-3");
+  TaskArray.push(taskToAdd);
 
-  const DIV = document.createElement("div");
-  DIV.classList.add("d-flex", "align-items-center");
+  const newTaskArray = JSON.stringify(TaskArray);
 
-  const SPAN1 = document.createElement("span");
-  SPAN1.innerText = taskToAdd;
-
-  const BUTTON = document.createElement("button");
-  BUTTON.classList.add(
-    "btn",
-    "btn-danger",
-    "ms-4",
-    "d-flex",
-    "gap-3",
-    "align-items-center",
-    "justify-content-center",
-  );
-  BUTTON.onclick = () => LI.remove();
-
-  const I = document.createElement("i");
-  I.classList.add("bi", "bi-trash");
-
-  const SPAN2 = document.createElement("span");
-  SPAN2.innerText = "Delete";
-
-  BUTTON.appendChild(I);
-  BUTTON.appendChild(SPAN2);
-
-  DIV.appendChild(SPAN1);
-  DIV.appendChild(BUTTON);
-
-  LI.appendChild(DIV);
-
-  document.getElementById("taskDiv").classList.remove("d-none");
-
-  document.getElementById("taskList").appendChild(LI);
-    
-  });
-
-
-  TaskArray.push(task)
-  localStorage.setItem("ToDoTask",TaskArray)
+  localStorage.setItem("toDoTask", newTaskArray);
 }
 
+function getTaskFromLocalStorage() {
+  const TaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
 
+  console.log(TaskArray);
+
+  TaskArray.forEach((element) => {
+    const LI = document.createElement("li");
+    LI.classList.add("my-3");
+
+    const DIV = document.createElement("div");
+    DIV.classList.add("d-flex", "align-items-center");
+
+    const SPAN1 = document.createElement("span");
+    SPAN1.innerText = element;
+
+    const BUTTON = document.createElement("button");
+    BUTTON.classList.add(
+      "btn",
+      "btn-danger",
+      "ms-4",
+      "d-flex",
+      "gap-3",
+      "align-items-center",
+      "justify-content-center",
+    );
+    BUTTON.onclick = () => {
+      LI.remove();
+      removeTaskFromLocalStorage(element);
+    };
+
+    const I = document.createElement("i");
+    I.classList.add("bi", "bi-trash");
+
+    const SPAN2 = document.createElement("span");
+    SPAN2.innerText = "Delete";
+
+    BUTTON.appendChild(I);
+    BUTTON.appendChild(SPAN2);
+
+    DIV.appendChild(SPAN1);
+    DIV.appendChild(BUTTON);
+
+    LI.appendChild(DIV);
+
+    document.getElementById("taskDiv").classList.remove("d-none");
+
+    document.getElementById("taskList").appendChild(LI);
+  });
+}
+
+getTaskFromLocalStorage();
+
+function removeTaskFromLocalStorage(taskItem) {
+  console.log(taskItem);
+
+  const TaskArray = JSON.parse(localStorage.getItem("toDoTask"));
+
+  console.log(TaskArray);
+
+  const newTaskArray = TaskArray.filter((item) => item !== taskItem);
+
+  console.log(newTaskArray);
+
+  const newTaskString = JSON.stringify(newTaskArray);
+
+  localStorage.setItem("toDoTask", newTaskString);
+}
---
+++ Projects/passwordManager/script.js
@@ -0,0 +1,36 @@
+document.getElementById("pmform").addEventListener("submit", (element) => {
+  element.preventDefault();
+
+  const SiteName = document.getElementById("siteName").value;
+  const UserName = document.getElementById("username").value;
+  const Password = document.getElementById("Password").value;
+
+  const DataPacket = {
+    siteName: SiteName,
+    username: UserName,
+    Password: Password,
+  };
+  console.log(DataPacket);
+  dataLocalStroage(DataPacket);
+  document.getElementById("siteName").value = "";
+  document.getElementById("username").value = "";
+  document.getElementById("Password").value = "";
+});
+
+function dataLocalStroage(data) {
+  const olddata = JSON.parse(localStorage.getItem("dataPacket")) || [];
+  console.log(olddata);
+
+  olddata.push(data);
+  console.log("string", olddata);
+
+  localStorage.setItem("dataPacket", JSON.stringify(olddata) );
+}
+
+document.getElementById("pmform").addEventListener("reset", (element) => {
+  element.preventDefault();
+
+  document.getElementById("siteName").value = "";
+  document.getElementById("username").value = "";
+  document.getElementById("Password").value = "";
+});
