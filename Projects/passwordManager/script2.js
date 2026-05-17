ba02a53ceb4e9867c16bd0c75d2113def1259843
codewithrsk <rsk800001@gmail.com>
2026-05-18 01:54:24 +0530

    updating password manager

--- Projects/passwordManager/script2.js
+++ Projects/passwordManager/script2.js
@@ -1,4 +1,30 @@
-function getdatafromlocalstroage(){
-    const data = localStorage.getItem("dataPacket")
-    
+function getdatafromlocalstroage() {
+  const data = JSON.parse(localStorage.getItem("dataPacket")) || [];
+
+  if (data <= 0) {
+    document.getElementById("passwordData").innerHTML = ` <tr>
+              <td colspan="3">No Data To Show</td>
+            </tr>`;
+    return;
+  }
+  console.log(data);
+
+  data.forEach((element) => {
+    const TR = document.createElement("tr");
+    TR.innerHTML = `<tr>
+              <td>${element.siteName}</td>
+              <td>${element.username}</td>
+              <td>${element.Password}</td>              
+            </tr>`;
+
+    passwordData.appendChild(TR);
+  });
+
+  //   document.getElementById("passwordData").innerHTML=`<tr>
+  //               <td>${data.siteName}</td>
+  //               <td>${data.username}</td>
+  //               <td>${data.Password}</td>
+  //             </tr>`;
 }
+
+getdatafromlocalstroage();
