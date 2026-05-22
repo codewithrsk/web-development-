37eafda615a44e62cf6e9db860ba11f95ebb527e
codewithrsk <rsk800001@gmail.com>
2026-05-22 23:32:38 +0530

    async/await

--- AdvanceJs/Promises/script.js
+++ AdvanceJs/Promises/script.js
@@ -2,7 +2,7 @@ function getData(value) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (value === 3) {
-        reject("Query Not Found")
+        reject("Query Not Found");
       } else {
         console.log("data Sent ", value);
         resolve("over solved");
@@ -16,3 +16,7 @@ getData(1)
   .then(() => getData(3))
   .then(() => getData(4))
   .then(() => getData(5));
+
+async function qwe() {
+  await getData(1);
+}
