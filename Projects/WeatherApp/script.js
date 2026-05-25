2df7e481134005854ada6b7ba9b657e266d099c8
codewithrsk <rsk800001@gmail.com>
2026-05-25 23:33:15 +0530

    update in weather App

--- Projects/WeatherApp/script.js
+++ Projects/WeatherApp/script.js
@@ -1,22 +1,46 @@
-const API_KEY = "use API KEY";
+const API_KEY = "API_KEY"
 
 async function getWeather() {
-  const cityNmae = document.getElementById("cityName").value.trim();
-  const { latitude, longitude } = await getGeoLocation(cityNmae);
+  const city = document.getElementById("cityName").value;
 
-  console.log(latitude);
-  console.log(longitude);
+
+   const { Lattitude, Longitude } = await getGeoLocation(city);
+
+  //   console.log({ Lattitude, Longitude });
+
+  const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${Lattitude}&lon=${Longitude}&appid=${API_KEY}`;
+
+  const response = await fetch(WEATHER_API);
+  const data = await response.json();
+
+  console.log(data);
+
+  const temperature = data.main.temp - 273.15;
+
+
   
+
+  document.getElementById("weatherBox").style.display = "block";
+
+  document.getElementById("city").innerText = data.name;
+
+  document.getElementById("temperature").innerText =  temperature.toFixed(2) ;
+
+  document.getElementById("humidity").innerText = data.main.humidity;
+
+  document.getElementById("wind").innerText = data.wind.speed;
 }
 
 async function getGeoLocation(city) {
-  const GEO_LOC_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${1}&appid=${API_KEY}`;
+  const GEO_LOC_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
 
-  const responce = await fetch(GEO_LOC_API);
+  const response = await fetch(GEO_LOC_API);
+  const data = await response.json();
 
-  const data = await responce.json();
-  const latitude = data[0].lat;
-  const longitude = data[0].lon;
+   console.log(data);
 
-  return { latitude, longitude };
+  const Lattitude = data[0].lat;
+  const Longitude = data[0].lon;
+
+  return { Lattitude, Longitude };
 }
