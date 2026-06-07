3769108b4f684e179953edbb454f36ecb7500959
codewithrsk <rsk800001@gmail.com>
2026-06-07 23:29:00 +0530

    refactor Header component for improved structure and styling; remove unnecessary divs and classes

--- Frontend/cravingByReact/src/components/Header.jsx
+++ Frontend/cravingByReact/src/components/Header.jsx
@@ -1,39 +1,51 @@
 import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg sticky-top  ">
-        <div className="container mx-2 w-100 ">
-          <Link
-            to={"/"}
-            className="navbar-brand text-white d-flex justify-content-start m-0 p-0"
-          >
-            <img
-              src="https://cravings.ricr.in/assets/transparentLogoLight-De2Z7I01.png"
-              alt="Cravings Logo"
-              width="100"
-              height="auto"
-              className="text-start"
-            />
-          </Link>
+    <nav className="navbar navbar-expand-lg sticky-top">
+      <div className="container">
+        <Link
+          to="/"
+          className="navbar-brand text-white m-0 p-0"
+        >
+          <img
+            src="https://cravings.ricr.in/assets/transparentLogoLight-De2Z7I01.png"
+            alt="Cravings Logo"
+            width="100"
+          />
+        </Link>
 
-          <div className="collapse navbar-collapse" id="nav">
-            <div className="navbar-nav ms-auto">
-              <div className="nav-item">
-                <Link to={"/login"} className="nav-link text-white">
-                  Login
-                </Link>
-              </div>
-              <dev className="nav-item">
-                <Link to={"/register"} className="btn btn-outline-light ms-2">
-                  Register
-                </Link>
-              </dev>
-            </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#nav"
+          aria-controls="nav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon">qwerthjhgfdfhnm</span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="nav">
+          <div className="navbar-nav ms-auto align-items-center">
+            <Link
+              to="/login"
+              className="nav-link text-white me-2"
+            >
+              Login
+            </Link>
+
+            <Link
+              to="/register"
+              className="btn btn-outline-light"
+            >
+              Register
+            </Link>
           </div>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
 
