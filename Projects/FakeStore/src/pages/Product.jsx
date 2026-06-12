3924407b63c38df9e6595a61cce3614269de4aed
codewithrsk <rsk800001@gmail.com>
2026-06-12 20:38:56 +0530

    Refactor code structure for improved readability and maintainability

--- Projects/FakeStore/src/pages/Product.jsx
+++ Projects/FakeStore/src/pages/Product.jsx
@@ -1,42 +1,71 @@
-import { MdOutlinePriceChange } from "react-icons/md";
+import React, { useEffect, useState } from "react";
 
-// fetch("https://fakestoreapi.com/products")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+import loading from "../assets/loading.gif";
 
-const Product = () => {
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchProducts = async () => {
+    try {
+      setIsLoading(true);
+
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setProducts(data);
+    } catch (error) {
+      setIsError(true);
+      setErrorMessage(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <div className="p-10 grid-cols-4">
-        <div className="w-75 border rounded h-auto ">
-            <div className="m-3">
-          <div>
-            <img
-              src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
-              alt=""
-              className="h-40 object-cover m-auto"
-            />
-          </div>
-          <div className="text-black font-bold text-xl">
-            Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
-          </div>
-          <div className="m-2 font-bold text-2xl flex items-center ">
-            <MdOutlinePriceChange className="m-2" /> 109.95
-          </div>
-          <div className="w-full flex justify-center m-2">
-            <button className=" px-5 py-3 bg-amber-400 rounded  ">
-              Add to Cart
-            </button>
-          </div>
-          <div className="object-fill">
-            our perfect pack for everyday use and walks in the forest. Stash
-            your laptop (up to 15 inches) in the padded sleeve, your everyday
-          </div>
+      {isLoading ? (
+        <div className="w-full h-[90vh] flex justify-center items-center">
+          <img src={loading} alt="" className="w-50" />
         </div>
-      </div>
-      </div>
+      ) : (
+        <div className="p-10 grid grid-cols-4 gap-5">
+          {products.length > 0 &&
+            products.map((product, index) => (
+              <div className="w-75 border rounded h-auto p-3" key={index}>
+                <div className="w-full h-40">
+                  <img
+                    src={product.image}
+                    alt=""
+                    className="w-full h-full object-contain"
+                  />
+                </div>
+                <div className="flex flex-col justify-between h-50">
+                  <div>
+                    <p className="text-xl">{product.title}</p>
+                    <p className="text-sm capitalize text-gray-500">
+                      {product.category}
+                    </p>
+                    <p>₹ {product.price * 100}</p>
+                    <p>{product.rating.rate}/5</p>
+                  </div>
+
+                  <button className="bg-orange-400 rounded-full px-4 py-2">
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            ))}
+        </div>
+      )}
+      
     </>
   );
 };
 
-export default Product;
+export default Products;
