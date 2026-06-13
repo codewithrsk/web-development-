821b63345a716116ae5eda083c62573534a86ca4
codewithrsk <rsk800001@gmail.com>
2026-06-13 19:12:17 +0530

    style: enhance product display with improved layout and hover effects

--- Projects/FakeStore/src/pages/Product.jsx
+++ Projects/FakeStore/src/pages/Product.jsx
@@ -31,23 +31,27 @@ const Products = () => {
     <>
       {isLoading ? (
         <div className="w-full h-[90vh] flex justify-center items-center">
-          <img src={loading} alt="" className="w-50" />
+          <img src={loading} alt="" className="w-50 " />
         </div>
       ) : (
         <div className="p-10 grid grid-cols-4 gap-5">
           {products.length > 0 &&
             products.map((product, index) => (
-              <div className="w-75 border rounded h-auto p-3" key={index}>
+              <div className="w-75 border rounded h-100 p-3" key={index}>
                 <div className="w-full h-40">
                   <img
                     src={product.image}
                     alt=""
-                    className="w-full h-full object-contain"
+                    className="w-full h-full object-contain hover:scale-120 hover:shadow-gray-400"
                   />
                 </div>
                 <div className="flex flex-col justify-between h-50">
                   <div>
-                    <p className="text-xl">{product.title}</p>
+                    <p className="text-xl" title={product.title}>
+                      {product.title.length > 50
+                        ? product.title.slice(0, 47) + "..."
+                        : product.title}
+                    </p>
                     <p className="text-sm capitalize text-gray-500">
                       {product.category}
                     </p>
@@ -55,7 +59,7 @@ const Products = () => {
                     <p>{product.rating.rate}/5</p>
                   </div>
 
-                  <button className="bg-orange-400 rounded-full px-4 py-2">
+                  <button className="bg-orange-400 rounded-full px-4 py-2 hover:bg-orange-600">
                     Add to cart
                   </button>
                 </div>
@@ -63,7 +67,6 @@ const Products = () => {
             ))}
         </div>
       )}
-      
     </>
   );
 };
