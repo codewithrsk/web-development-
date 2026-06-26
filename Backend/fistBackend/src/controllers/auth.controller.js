8f427411711fa18f663e1c7980d9d592a98d333d
codewithrsk <rsk800001@gmail.com>
2026-06-26 18:49:08 +0530

    fix: improve error handling and formatting in auth controller

--- Backend/fistBackend/src/controllers/auth.controller.js
+++ Backend/fistBackend/src/controllers/auth.controller.js
@@ -1,20 +1,22 @@
 import error from "mongoose/lib/error/index.js";
 import User from "../models/user.model.js";
+import error from "mongoose/lib/error/index.js";
 
-export const Register = async (req, res,next) => {
+export const Register = async (req, res, next) => {
   try {
     const { fullName, email, password, phone, gender, dob } = req.body;
 
     if (!fullName || !email || !password || !phone || !gender || !dob) {
-     const error = new Error("All Filed Requred")
-     error.statusCode=400
+      const error = new Error("All fields Required");
+      error.statusCode = 400;
       return next(error);
     }
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      res.status(409).json({ message: "User Already Registerd" });
-      return;
+      const error = new Error("Email already registred");
+      error.statusCode = 409;
+      return next(error);
     }
 
     const photourl = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
@@ -35,7 +37,7 @@ export const Register = async (req, res,next) => {
 
     res.status(201).json({ message: "user created Successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Interal Server Error" });
+    next(error);
   }
 };
 
@@ -43,14 +45,16 @@ export const Loginuser = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      res.status(400).data({ message: "Bad Request" });
-      return;
+     const error = new Error("All fields Required");
+      error.statusCode = 400;
+      return next(error);
     }
 
     const findUser = await User.findOne({ email });
     if (!existingUser) {
-      res.status(401).json({ message: "Not Rigester User" });
-      return;
+      const error = new error("Not Rigester User")
+      error.status(401)
+      return next(error);
     }
 
     res.status(200).json({ message: "login Succesfull" });
